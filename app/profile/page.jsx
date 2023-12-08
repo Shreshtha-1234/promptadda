1f0dfd4afd0863afdcc2b6fd35bca109118342b8
Shreshtha <shdhtha7860@gmail.com>
2023-12-08 18:06:40 +0530

    merge conflict resolved by ikaushiksharma

--- app/profile/page.jsx
+++ app/profile/page.jsx
@@ -1,66 +1,64 @@
-'use client'
-import { useState , useEffect } from "react"
-import { useSession } from "next-auth/react"
-import { useRouter } from "next/navigation"
-import Profie from "@components/Profie"
+"use client";
+import { useState, useEffect } from "react";
+import { useSession } from "next-auth/react";
+import { useRouter } from "next/navigation";
+import Profie from "@components/Profie";
 
 const Profile = () => {
-    const {data : session} = useSession();
-    const [ d, setD] = useState();
-  
-    const router = useRouter();
-    const [posts , setPosts] = useState([]);
-    useEffect( ()=>{
-        const fetchPost = async function(){  
-          try{   
-            const response = await fetch(`/api/users/${session?.user.id}/posts`);
-            const data = await response.json();
-            setPosts(data);
-            
-          } catch(err){
-            console.log("error occured in fetching prompts",err);
-          }
-        }
-        if(session?.user.id){
-            fetchPost();
-        }
-       
-      }, [session]);
+  const { data: session } = useSession();
 
-    const handleEdit = (post) => {
-        router.push(`/update-prompt?id=${post._id}`);
+  const router = useRouter();
+  const [posts, setPosts] = useState([]);
+  useEffect(() => {
+    const fetchPost = async function () {
+      try {
+        const response = await fetch(`/api/users/${session?.user.id}/posts`);
+        const data = await response.json();
+        setPosts(data);
+      } catch (err) {
+        console.log("error occured in fetching prompts", err);
+      }
+    };
+    if (session?.user.id) {
+      fetchPost();
+    } else {
+      return router.push("/");
     }
+  }, [session]);
 
-    const handleDelete = async (post) => {
-        const hasConfirmed = confirm("Are you sure you want to delete the prompt ? ");
-        if(hasConfirmed)
-        {
-            try{
-                const res = await fetch(`api/prompt/${post._id}` ,{
-                    method: 'DELETE'
-                })
-            }
-            catch(err){
-                console.log(err);
-            }
-            const filterPost = posts.filter((p)=>p._id !== post._id);
-            setPosts(filterPost);
-        }
+  const handleEdit = (post) => {
+    router.push(`/update-prompt?id=${post._id}`);
+  };
+
+  const handleDelete = async (post) => {
+    const hasConfirmed = confirm(
+      "Are you sure you want to delete the prompt ? "
+    );
+    if (hasConfirmed) {
+      try {
+        const res = await fetch(`api/prompt/${post._id}`, {
+          method: "DELETE",
+        });
+      } catch (err) {
+        console.log(err);
+      }
+      const filterPost = posts.filter((p) => p._id !== post._id);
+      setPosts(filterPost);
     }
-    if(!session?.user)
-    router.push("/")
+  };
+
   return (
-    
     <div>
-        <Profie
+      <Profie
         name="My"
         desc="Welcome to your personalized profile page"
         data={posts}
         handleEdit={handleEdit}
         handleDelete={handleDelete}
-        />
+      />
     </div>
-  )
-}
+  );
+};
 
 export default Profile
+;
