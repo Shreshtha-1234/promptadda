6453778b155de53b81c1c0e719cbae20e53fd047
Shreshtha <shdhtha7860@gmail.com>
2023-12-08 18:45:55 +0530

    cache issue

--- components/feed.jsx
+++ components/feed.jsx
@@ -4,49 +4,56 @@ import PromptBox from "./PromptBox";
 import PromptCardList from "./PromptCardList";
 
 const Feed = () => {
-  const [search , setSearch ] = useState("");
-  const [posts , setPosts] = useState([]);
-  const [original , setOriginal] = useState([]);
+  const [search, setSearch] = useState("");
+  const [posts, setPosts] = useState([]);
+  const [original, setOriginal] = useState([]);
 
-  
-  useEffect( ()=>{
-    const fetchPost = async function(){
-      try{
-        const response = await fetch("/api/prompt");
+  useEffect(() => {
+    const fetchPost = async function () {
+      try {
+        const response = await fetch("/api/prompt", { cache: "no-cache" });
         const data = await response.json();
         setPosts(data);
         setOriginal(data);
-      } catch(err){
+      } catch (err) {
         console.log("error occured in fetching prompts");
       }
-    }
+    };
     fetchPost();
   }, []);
-  
+
   function searchHelper(searchValue) {
-      const filteredPost = original.filter((pst) => {
-        return pst.prompt.toLowerCase().includes(searchValue) || pst.creator.username.toLowerCase().includes
-        (searchValue) || pst.tag.toLowerCase().includes(searchValue)
-    })
+    const filteredPost = original.filter((pst) => {
+      return (
+        pst.prompt.toLowerCase().includes(searchValue) ||
+        pst.creator.username.toLowerCase().includes(searchValue) ||
+        pst.tag.toLowerCase().includes(searchValue)
+      );
+    });
     setPosts(filteredPost);
   }
 
-
-  function handleTagclick(val){
-      setSearch(val);
-      searchHelper(val.toLowerCase());
+  function handleTagclick(val) {
+    setSearch(val);
+    searchHelper(val.toLowerCase());
   }
 
   return (
     <div>
       <section className="feed">
         <form className="relative w-full flex-center">
-          <input type="text" placeholder="search for a tag or username"
-          value={search}
-          onChange={(e)=>{setSearch(e.target.value);searchHelper(e.target.value.toLowerCase()) }} 
-          className="search_input peer"/>
+          <input
+            type="text"
+            placeholder="search for a tag or username"
+            value={search}
+            onChange={(e) => {
+              setSearch(e.target.value);
+              searchHelper(e.target.value.toLowerCase());
+            }}
+            className="search_input peer"
+          />
         </form>
-        <PromptCardList data={posts} handleTagclick={handleTagclick}/>
+        <PromptCardList data={posts} handleTagclick={handleTagclick} />
       </section>
     </div>
   );
