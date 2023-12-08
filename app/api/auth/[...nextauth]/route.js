d6437fb1bc5fb873d394a71a93fe24fbae6140d4
Shreshtha <shdhtha7860@gmail.com>
2023-12-08 18:36:28 +0530

    secret added

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -9,6 +9,7 @@ const handler = nextAuth({
             clientSecret:process.env.GOOGLE_CLIENT_SECRET,
         })
     ],
+    secret:process.env.NEXT_AUTH_SECRET,
     callbacks: {
         async session({session}) {
             const sessionUser = await User.findOne({
